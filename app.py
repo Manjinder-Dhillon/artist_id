86873531ec04e56994829978320ffe8f567bc0e3
Manjinder <mkaur10@student.gsu.edu>
2021-12-01 06:47:02 +0000

    added google login

--- app.py
+++ app.py
@@ -201,8 +201,8 @@ def index():
 
 
 app.run(
-    debug=True
-    # host='0.0.0.0',
-    # host=os.getenv("IP", "0.0.0.0"),
-    # port=int(os.getenv("PORT", "8080")),
+    # debug=True
+    host='0.0.0.0',
+    host=os.getenv("IP", "0.0.0.0"),
+    port=int(os.getenv("PORT", "8080")),
 )
