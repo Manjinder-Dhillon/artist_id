86932a3fa92398da6482acf5ca1e623e49accdcd
Manjinder <mkaur10@student.gsu.edu>
2021-12-01 06:57:15 +0000

    added google login

--- app.py
+++ app.py
@@ -21,15 +21,6 @@ load_dotenv(find_dotenv())
 
 app = flask.Flask(__name__)
 
-global client_id
-global client_secret
-
-client_Id = os.getenv("client_Id")
-client_Secret = os.getenv("client_Secret")
-# secret key
-secret_key = os.getenv("secret_key")
-app.config["SECRET_KEY"] = secret_key
-
 # database url
 url = os.getenv("DATABASE_URL")
 if url and url.startswith("postgres://"):
@@ -40,28 +31,6 @@ app.config["SQLALCHEMY_DATABASE_URI"] = url
 # secret key
 secret_key = os.getenv("secret_key")
 app.config["SECRET_KEY"] = secret_key
-
-app.config['SESSION_COOKIE_NAME'] = 'google-login-session'
-app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=5)
-
-
-
-
-# oAuth Setup
-oauth = OAuth(app)
-google = oauth.register(
-    name='google',
-    client_id=client_Id,
-    client_secret=client_Secret,
-    access_token_url='https://accounts.google.com/o/oauth2/token',
-    access_token_params=None,
-    authorize_url='https://accounts.google.com/o/oauth2/auth',
-    authorize_params=None,
-    api_base_url='https://www.googleapis.com/oauth2/v1/',
-    userinfo_endpoint='https://openidconnect.googleapis.com/v1/userinfo',  # This is only needed if using openId to fetch user info
-    client_kwargs={'scope': 'openid email profile'},
-)
-
 
 db = SQLAlchemy(app)
 
@@ -98,8 +67,6 @@ def sign():
 @app.route('/sign', methods=['POST'])
 def sign_post():
     username = flask.request.form.get('username')
-   
-
     user = Todo.query.filter_by(username=username).first()
 
     if user:
@@ -114,14 +81,6 @@ def sign_post():
     return redirect(url_for('login'))
 
 # login page
-@app.route('/login')
-def googleLogin():
-    google = oauth.create_client('google')  # create the google oauth client
-    redirect_uri = url_for('authorize', _external=True)
-    return google.authorize_redirect(redirect_uri)
-      
-
-
 @app.route("/")
 def login():
     return flask.render_template("login.html")
@@ -140,16 +99,7 @@ def login_post():
         flash(u'Invalid login provided. Click on Signup now to create', 'error')
         return redirect(url_for('login'))
 
-@app.route('/authorize')
-def authorize():
-    google = oauth.create_client('google')  # create the google oauth client
-    token = google.authorize_access_token()  # Access token from google (needed to get user info)
-    resp = google.get('userinfo')  # userinfo contains stuff u specificed in the scrope
-    user_info = resp.json()
-    user = oauth.google.userinfo()  # uses openid endpoint to fetch user info
-    session['profile'] = user_info
-    session.permanent = True  # make the session permanant so it keeps existing after broweser gets closed
-    return redirect('/artist')
+
 # artist page
 @app.route("/artist", methods=["GET", "POST"])  # Python decorator
 def get_artist_id():
@@ -202,7 +152,7 @@ def index():
 
 app.run(
     # debug=True
-    host='0.0.0.0',
+    # host='0.0.0.0',
     host=os.getenv("IP", "0.0.0.0"),
     port=int(os.getenv("PORT", "8080")),
 )
